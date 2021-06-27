cff9c29e229e9099e55bb01300e48742252fcc9b
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-06-27 10:14:03 +0200

    set up eslint and prittier

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>Search goes here!</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>Our react native blank canvas!</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  search: {
+    backgroundColor: "green",
+    width: "100%",
+    padding: 16,
+  },
+  list: {
+    backgroundColor: "blue",
+    flex: 1,
+    width: "100%",
+    padding: 16,
   },
 });
