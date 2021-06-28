9b8b655ca63d25ba978c4b95663aa3021a96b98c
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-06-28 17:46:16 +0200

    fixed bug with fonts and added svg icons

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
-
+import AppLoading from "expo-app-loading";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -11,16 +11,16 @@ import {
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
-  let [oswaldLoaded] = useOswald({
+  const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
 
-  let [latoLoaded] = useLato({
+  const [latoLoaded] = useLato({
     Lato_400Regular,
   });
 
-  if (!oswaldLoaded && !latoLoaded) {
-    return null;
+  if (!oswaldLoaded || !latoLoaded) {
+    return <AppLoading />;
   }
 
   return (
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,32 +1,74 @@
 import React from "react";
-import { Text } from "react-native";
-import { Card, Title, Paragraph } from "react-native-paper";
+import { Text, View, Image } from "react-native";
+import { Card, Title } from "react-native-paper";
 import styled from "styled-components/native";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 
 const CardTitle = styled(Title)`
   color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+`;
+
+const Address = styled(Text)`
   font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Section = styled(View)`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+  justify-content: space-between;
+`;
+
+const Rating = styled(View)`
+  flex-direction: row;
+`;
+
+const Icon = styled(View)`
+  flex-direction: row;
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://cdn.pixabay.com/photo/2016/11/18/14/05/brick-wall-1834784_1280.jpg",
     ],
     address = "100 random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily = false,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <Card>
       <Card.Cover source={{ uri: photos[0] }} />
       <Card.Content>
         <CardTitle>{name}</CardTitle>
-        <Paragraph>Card content</Paragraph>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <Icon>
+            {isClosedTemporarily && (
+              <Text varuant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+          </Icon>
+        </Section>
+        <Address>{address}</Address>
       </Card.Content>
     </Card>
   );
