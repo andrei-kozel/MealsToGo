0db104666d58d48454e2995e718400ac6c511697
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-07-01 15:15:37 +0200

    transform data

--- src/services/restaurant/restaurants.service.js
+++ src/services/restaurant/restaurants.service.js
@@ -11,11 +11,18 @@ const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   });
 };
 
-const restaurantsTransform = (result) => {
-  return camelize(result);
+const restaurantsTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosed: restaurant.business_status === "CLOSED_TEMPORARILY",
+    };
+  });
+  return camelize(mappedResults);
 };
 
 restaurantsRequest()
   .then(restaurantsTransform)
-  .then((result) => console.log(camelize(result)))
+  .then((result) => console.log(result))
   .catch((err) => console.log("error:", err));
