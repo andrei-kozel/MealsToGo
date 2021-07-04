a3e224a78bdb11d12493aa2cd45e9594a2673f67
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-07-04 22:29:18 +0200

    rendering map markers

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
+
 import { SearchBar } from "../components/search.component";
+
+import { RestaurantsContext } from "../../../services/restaurant/restaurants.context";
+import { LocationContext } from "../../../services/location/location.context";
 
 const Map = styled(MapView)`
   height: 100%;
@@ -9,10 +13,44 @@ const Map = styled(MapView)`
 `;
 
 export const MapScreen = () => {
+  const { restaurants = [] } = useContext(RestaurantsContext);
+  const { location } = useContext(LocationContext);
+  const { lat, lng, viewport } = location;
+
+  const [latDelta, setLatDelta] = useState(0);
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    const ld = northeastLat - southwestLat;
+    setLatDelta(ld);
+  }, [location, viewport]);
+
   return (
     <>
       <SearchBar />
-      <Map />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return (
+            <MapView.Marker
+              key={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
+              title={restaurant.name}
+            />
+          );
+        })}
+      </Map>
     </>
   );
 };
