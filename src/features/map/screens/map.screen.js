00496272594d45daaf837d63c97a1fafaf8d1fbc
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-07-06 16:38:11 +0200

    onPress function

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -13,7 +13,7 @@ const Map = styled(MapView)`
   width: 100%;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
   const { restaurants = [] } = useContext(RestaurantsContext);
   const { location } = useContext(LocationContext);
   const { lat, lng, viewport } = location;
@@ -49,7 +49,11 @@ export const MapScreen = () => {
               }}
               title={restaurant.name}
             >
-              <MapView.Callout>
+              <MapView.Callout
+                onPress={() =>
+                  navigation.navigate("RestaurantDetail", { restaurant })
+                }
+              >
                 <MapCallout restaurant={restaurant} />
               </MapView.Callout>
             </MapView.Marker>
