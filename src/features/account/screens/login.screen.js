300b2123542d72154414feb0f6b84ccb35020337
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-07-10 11:15:31 +0200

    login function

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,6 +1,58 @@
-import React from "react";
-import { AccountBackground } from "../components/account.styles";
+import React, { useState, useContext } from "react";
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+  AuthInput,
+} from "../components/account.styles";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+import { Spacer } from "../../../components/spacer.component";
+import { Text } from "../../../components/text.component";
 
 export const LoginScreen = () => {
-  return <AccountBackground />;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { onLogin, error } = useContext(AuthenticationContext);
+
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <AccountContainer>
+        <AuthInput
+          label="Email"
+          value={email}
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(text) => setEmail(text)}
+        />
+        <AuthInput
+          label="Password"
+          value={password}
+          textContentType="password"
+          secureTextEntry
+          autoCapitalize="none"
+          secure
+          onChangeText={(text) => setPassword(text)}
+        />
+        {error && (
+          <Spacer>
+            <Text variant="error">{error}</Text>
+          </Spacer>
+        )}
+        <Spacer size="large">
+          <AuthButton
+            icon="lock-open-outline"
+            mode="contained"
+            color="black"
+            disabled={email.length > 0 && password.length > 0 ? false : true}
+            onPress={() => onLogin(email, password)}
+          >
+            Login
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
+    </AccountBackground>
+  );
 };
--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -7,24 +7,25 @@ export const AuthenticationContext = createContext();
 export const AuthenticationContextProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState([]);
 
   const onLogin = (email, password) => {
     setIsLoading(true);
-    loginRequest
-      .then(email, password)
+    loginRequest(email, password)
       .then((u) => {
         setUser(u);
         setIsLoading(false);
       })
       .catch((e) => {
         setIsLoading(false);
-        setError(e);
+        setError(e.toString());
       });
   };
 
   return (
-    <AuthenticationContext.Provider value={{ user, isLoading, error, onLogin }}>
+    <AuthenticationContext.Provider
+      value={{ isAuthenticated: !!user, user, isLoading, error, onLogin }}
+    >
       {children}
     </AuthenticationContext.Provider>
   );
